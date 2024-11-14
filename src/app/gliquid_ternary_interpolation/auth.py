a9c674bdea694f7e15fc3b3a65ed1c7fab99709d
Nicholas David <59184306+nrdavid@users.noreply.github.com>
2024-11-14 17:21:00 -0500

    Revert "Added G-liquid ternary visualization app (#3)"
    This reverts commit d3ec4de32aca84dcea1f4f7002bc9f2fd93dcdb2.

--- src/app/gliquid_ternary_interpolation/auth.py
+++
@@ -1,4 +0,0 @@
-key = "Rtb4ppAs9rcNVzh10IVdBRh6HwlBymcJ"
-MPDS_api_key = "29r3PRLsIW8YNzxxTCVgDmxP0ylZqfWSUvBVE3IVyrZvpqKG"
-New_MP_api_key = "Jcw46im7UV1xOfHzbZZ8nkq8BH00Pf6s"
-Legacy_MP_api_key = "2d5wyVmhDCpPMAkq"
