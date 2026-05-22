6db79f56955d00f9d17bcef8dd503e63c85d374e
GeorgeZJT <zhgeorge@umich.edu>
2026-05-22 19:25:16 -0400

    added fermi surface and tomography VR visualization

---
+++ src/app/vr/fermi/assets/fermi_George_MgB2_-2-L7SRM6eG.js
@@ -0,0 +1,1 @@
+const e=""+new URL("fermi_George_MgB2_-2-BKjVS0WA.json",import.meta.url).href;export{e as default};
---
+++ src/app/vr/fermi/assets/fermi_George_MgB2_-5-B1u8fG6V.js
@@ -0,0 +1,1 @@
+const e=""+new URL("fermi_George_MgB2_-5-huqX0fD2.json",import.meta.url).href;export{e as default};
